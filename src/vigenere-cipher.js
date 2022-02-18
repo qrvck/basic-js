67c5d33841b01471119f1f69907b3f2a3a42f93b
qrvck <v.kapitan2010@gmail.com>
2022-02-18 04:29:53 +0300

    Vigenere cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,58 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(mode) {
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    this.mode = mode
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let encr = '';
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+    let ii = 0;  // 6
+
+    for (let i = 0; i < message.length; i++) {
+      if (!this.alphabet.includes(message[i])) {
+        encr = encr + message[i];
+      } else {
+        let indexOfMessage = this.alphabet.indexOf(message[i]);
+        let indexOfKey = this.alphabet.indexOf(key[ii % key.length])
+        encr = encr + this.alphabet[(indexOfMessage + indexOfKey) % 26];
+        ii++;
+      }
+    }
+    if (this.mode === false) return encr.split('').reverse().join('')
+    return encr
   }
+
+  decrypt(encryptedMessage, key) {
+    if (encryptedMessage === undefined || key === undefined) throw new Error('Incorrect arguments!');
+
+    let decr = '';
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+    let ii = 0;
+
+    for (let i = 0; i < encryptedMessage.length; i++) {
+      if (!this.alphabet.includes(encryptedMessage[i])) {
+        decr = decr + encryptedMessage[i];
+      } else {
+        let indexOfEncryptedMessage = this.alphabet.indexOf(encryptedMessage[i]);
+        let indexOfKey = this.alphabet.indexOf(key[ii % key.length]);
+
+        let indexOfDecr = (indexOfEncryptedMessage - indexOfKey >= 0) ? indexOfEncryptedMessage - indexOfKey % 26 : 26 + (indexOfEncryptedMessage - indexOfKey);
+        decr = decr + this.alphabet[indexOfDecr];
+        ii++
+      }
+    }
+    if (this.mode === false) return decr.split('').reverse().join('')
+    return decr
+  }
+
 }
 
 module.exports = {
